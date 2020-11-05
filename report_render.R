a983f12a21286ae2f6fd0f2f3a155dc4f51019fe
AndrewLangvt <andrew.lang.vt@gmail.com>
2020-11-05 12:28:24 -0500

    Update report_render.R

--- report_render.R
+++ report_render.R
@@ -14,6 +14,15 @@ status_table <- read.table(file_path_as_absolute(args[3]), header = T, check.nam
 report <- file_path_as_absolute(args[4])
 location <- args[5]
 
+# Modify SNP matrix to pairwise list
+pairwise <- t(combn(colnames(snp_mat), 2))
+pairwise_list <- data.frame(pairwise, dist=snp_mat[pairwise])
+#xy <- t(combn(colnames(m), 2))
+#data.frame(xy, dist=m[xy])
+rownames(pairwise_list) <- NULL
+colnames(pairwise_list) <- c("Sample1","Sample2","Distance")
+write.csv(pairwise_list, 'pairwise_snp_list.csv', row.names = FALSE, quote = FALSE)
+
 # Render the report
 #render(report, output_dir=location, output_file='report.pdf', knit_root_dir=location, intermediates_dir=location)
 render(report, output_file='report.pdf')
